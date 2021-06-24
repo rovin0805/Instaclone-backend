8ed3e380895f4d863ece7c7cf75a010f8d388b4f
rovin0805 <qhdrn67@naver.com>
2021-06-24 21:50:05 +0900

    creating account

---
+++ users/users.mutations.js
@@ -0,0 +1,38 @@
+import client from "../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      // check if username or email are already on DB
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+      // hash passsword
+      const uglyPassword = await bcrypt.hash(password, 10);
+      // save and return the user
+      return client.user.create({
+        data: {
+          username,
+          firstName,
+          lastName,
+          email,
+          password: uglyPassword,
+        },
+      });
+    },
+  },
+};
