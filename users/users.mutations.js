76c19f8cc90a2262f34bac291f687b3a5a615e5b
rovin0805 <qhdrn67@naver.com>
2021-06-24 23:03:16 +0900

    login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import client from "../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -37,6 +38,19 @@ export default {
       } catch (error) {
         return error;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find an user with args.username
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) return { ok: false, error: "User Not Found." };
+
+      // check a password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) return { ok: false, error: "Incorrect password." };
+
+      // issue a token and send it to the user
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return { ok: true, token };
     },
   },
 };
