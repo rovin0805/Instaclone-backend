549921a2471352e7234e0e06f0139bb4e30374cd
rovin0805 <qhdrn67@naver.com>
2021-06-28 20:02:15 +0900

    fixed erros & computed fields

--- users/seeFollowing/seeFollowing.resolvers.js
+++ users/seeFollowing/seeFollowing.resolvers.js
@@ -1,12 +1,31 @@
-import { gql } from "apollo-server";
+import client from "../../client";
 
-export default gql`
-  type SeeFollowingResult {
-    ok: Boolean!
-    error: String
-    following: [User]
-  }
-  type Query {
-    seeFollowing(username: String!, lastId: Int): SeeFollowingResult!
-  }
-`;
+export default {
+  Query: {
+    seeFollowing: async (_, { username, lastId }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+
+      const following = await client.user
+        .findUnique({ where: { username } })
+        .following({
+          take: 5,
+          skip: lastId ? 1 : 0,
+          ...(lastId && { cursor: { id: lastId } }),
+        });
+
+      return {
+        ok: true,
+        following,
+      };
+    },
+  },
+};
--- users/seeFollowing/seeFollowing.typeDefs.js
+++ users/seeFollowing/seeFollowing.typeDefs.js
@@ -1,31 +1,12 @@
-import client from "../../client";
+import { gql } from "apollo-server";
 
-export default {
-  Query: {
-    seeFollowing: async (_, { username, lastId }) => {
-      const ok = await client.user.findUnique({
-        where: { username },
-        select: { id: true },
-      });
-      if (!ok) {
-        return {
-          ok: false,
-          error: "User not found",
-        };
-      }
-
-      const following = await client.user
-        .findUnique({ where: { username } })
-        .following({
-          take: 5,
-          skip: lastId ? 1 : 0,
-          ...(lastId && { cursor: { id: lastId } }),
-        });
-
-      return {
-        ok: true,
-        following,
-      };
-    },
-  },
-};
+export default gql`
+  type SeeFollowingResult {
+    ok: Boolean!
+    error: String
+    following: [User]
+  }
+  type Query {
+    seeFollowing(username: String!, lastId: Int): SeeFollowingResult!
+  }
+`;
